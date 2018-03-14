8153caffc91f38f4bb880fcc2bb4d3437d1508d4
yinguowei <yinguowei@gmail.com>
2018-03-14 22:07:28 +0800

    first inint!

---
+++ src/main/java/cn/yinguowei/admin/YinAdminApplication.java
@@ -0,0 +1,12 @@
+package cn.yinguowei.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YinAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YinAdminApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/yinguowei/admin/YinAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.yinguowei.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YinAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
